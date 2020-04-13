690eab4d3acce7abde9d413aa8ff85688f07dfaa
nmxi <mikki1118_2@hotmail.co.jp>
2020-04-13 14:28:36 +0900

    v1.1.0

---
+++ Assets/FaciaSDK/SampleScript/Sample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace nmxi.facia.receiver.sample
+{
+    public class Sample : MonoBehaviour
+    {
+        [SerializeField] private Text _targetText;
+
+        public void UpdateText(string s)
+        {
+            _targetText.text = s;
+        }
+    }
+}
